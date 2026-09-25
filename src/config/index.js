6768d77e955c9364002386399fe9b39074d191f3
Yusuf Habu <yuhyam95@gmail.com>
2026-09-25 12:08:36 +0100

    Made changes to connect .env

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
-require('dotenv').config();
 const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../../.env') });
 
 module.exports = {
   get db() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const db = require('./db/index.js');
 const climdesService = require('./services/climdes.js');
 const tahmoService = require('./services/tahmo.js');
-require('dotenv').config();
 
 /**
  * Main periodic sync function
